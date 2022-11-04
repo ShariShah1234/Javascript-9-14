3b17e243f9f9031752d4568466a8671500cd7f52
ShariShah1234 <muhammadshaheryar74@gmail.com>
2022-11-04 15:19:39 +0500

    commit

---
+++ app.js
@@ -0,0 +1,162 @@
+// TASK 9-14
+
+
+// user input
+
+// var ab = prompt()
+
+// console.log(ab)
+
+
+// var Shari = prompt()
+// console.log(Shari)
+
+// var cd = prompt()
+// console.log(cd )
+
+// var eg = prompt()
+// console.log(eg)
+
+
+// var FirstName=prompt("enter first name:")
+// var LastName=prompt("enter last name")
+// var city=prompt("Enter your City","karachi")
+// console.log(FirstName)
+// console.log(LastName)
+// console.log(city)
+
+
+
+
+
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+// }
+// if (x !== "Vatican") {
+//  alert("Wrong answer");
+//  }
+
+
+
+
+
+
+
+// percentage program
+
+// var eng = +prompt("Enter the marks of English")
+// var phy = +prompt("Enter the marks of Physics")
+// var maths = +prompt("Enter the marks of Maths")
+// var discrete = +prompt("Enter the marks of Discrete")
+// var dsa = +prompt("Enter the marks of Data Structure")
+
+// console.log("Your English Marks are= "+(eng))
+// console.log(phy)
+// console.log(maths)
+// console.log(discrete)
+// console.log(dsa)
+
+// var total= eng+phy+maths+discrete+dsa;
+// console.log("your total marks"+(total))
+
+// var per=(total/500)*100
+
+// console.log("your percentage="+(per))
+
+
+// if(per>=70)
+// {
+//     console.log("A grade")
+// }
+
+// else if(per>=60)
+// {
+//     console.log("B grade")
+// }
+
+// else{
+//     console.log("fail")
+// }
+
+
+
+// conditions
+
+// var officer = false
+// var assi= false
+
+// if(officer==true)
+// {
+//     console.log("documents are submitted!")
+// }
+
+
+// else if(assi==true)
+// {
+//     console.log("documents are submitted to Assistant")
+// }
+
+
+// else{
+//     console.log("documents can not be submitted!")
+// }
+
+
+
+
+// ==(equal)
+// !=(not equal)
+// ===(equal with data type)
+// !==(not equal with data type)
+
+
+// var age=15
+// var studentcard=true
+
+// if(age==16){
+//     console.log("Allow")
+// }
+
+// else{
+
+//     if(studentcard==true)
+//     {
+//         console.log("Allow on student car")
+//     }
+
+//     else if(studentcard==true && age==15)
+//     {
+//         console.log("allow")
+//     }
+
+//     else{
+//     console.log("not Allow")
+//     }  
+// }
+
+
+
+// var nicAge=15;
+// var studentcard=true
+
+// if(nicAge==16){
+//     console.log("Allow")
+// }
+
+// else{
+
+//     if(studentcard==true)
+//     {
+//         console.log("Allow on student car")
+//     }
+
+//     else if(studentcard==true && age==15)
+//     {
+//         console.log("allow")
+//     }
+
+//     else{
+//     console.log("not Allow")
+//     }  
+// }
